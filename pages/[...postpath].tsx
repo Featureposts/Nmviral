a4bb9bbc02efecc889e66f2a7452f9cc6c1f7c38
Featureposts <133018251+Featureposts@users.noreply.github.com>
2023-06-18 20:29:37 +0500

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -18,7 +18,7 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 			redirect: {
 				permanent: false,
 				destination: `${
-					`https://girlsnotty.lovestoblog.com` + encodeURI(path as string)
+					`https://featureposts.com`
 				}`,
 			},
 		};
